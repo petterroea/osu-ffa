8b5e6ef09c07f35bc3118c10f9d9f12debaca1b2
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-10-02 01:50:55 +0200

    config stuff

--- ffa.js
+++ ffa.js
@@ -1,3 +1,12 @@
+var API_KEY = "XXX";
+var MATCH_ID = "XXX";
+var API_ENDPOINT = "https://osu.ppy.sh/api/";
+
 $(document).ready(function() {
-	
+	setInterval(fetchMatchData, 1000);
 });
+function fetchMatchData() {
+	$.getJSON(API_ENDPOINT + "get_match?k=" + API_KEY + "&mp=" + MATCH_ID, function(data) {
+
+	});
+}
